e6b0261f4c414a9544c3377ab938b72a3bd77c6f
AlbertoJTD <albertoJTD@outlook.com>
2021-09-29 13:30:02 -0500

    User authentication

--- test/controllers/design_systems_controller_test.rb
+++ test/controllers/design_systems_controller_test.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
 class DesignSystemsControllerTest < ActionDispatch::IntegrationTest
-  def should_get_index get design_systems_index_url assert_response :success end
+  def should_get_index
+    get design_systems_index_url
+    assert_response :success
+  end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  def should_get_index get home_index_url assert_response :success end
+  def should_get_index 
+    get home_index_url
+    assert_response :success
+  end
 end
